22f6b114fd1acdd7ae40d5229c1f18c8a09e56a8
salma8 <salma.rabia886@gmail.com>
2026-08-12 12:44:55 +0300

    initial portfolio

---
+++ hello.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+#this is a simple Python program that prints "Hello, World!" to the console.
+#this more than 1 line comment is used to explain the purpose of the code.
+#1.create user name and password
+#2. create a function to validate the user input
+# store the data in a dictionary
